cfbd70ba6857f10188f6ecd1ebb7c4aa451a8acd
JoDemVel <josuedemetriovelizescobar@gmail.com>
2024-01-04 14:20:54 -0400

    fill in button added

--- src/components/SaveOptions.jsx
+++ src/components/SaveOptions.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
+import { loadLocalStorage } from '../services/loader';
+import { getAllAlbums, getAllArtist } from "../services/localStorageHandler";
+import { useAlbumStore } from "../stores/useAlbumStore";
+import { useArtistStore } from "../stores/useArtistStore";
 
 import addMusicLogo from "../assets/addMusic.svg";
 import addAlbumLogo from "../assets/addAlbum.svg";
 import addArtistLogo from "../assets/addArtist.svg";
+import fillIn from "../assets/fillIn.svg";
 
 import ModalForm from "./ModalForm";
 
 const SaveOptions = () => {
   const [typeForm, setTypeForm] = useState('');
+
+  const { setAlbums } = useAlbumStore();
+  const { setArtists } = useArtistStore();
+
+  const loadData = () => {
+    if (!localStorage.getItem('artists')) {
+      loadLocalStorage();
+    }
+
+    setAlbums(getAllAlbums(true));
+    setArtists(getAllArtist());
+  }
 
   return (
     <div className="join join-horizontal lg:join-horizontal">
@@ -19,6 +36,9 @@ const SaveOptions = () => {
       </button>
       <button className="btn join-item" onClick={()=>{setTypeForm('artist'); document.getElementById('form_modal').showModal()}}>
         <img className="w-7 h-7" src={addArtistLogo} alt="add Artist" />
+      </button>
+      <button className="btn join-item" onClick={loadData}>
+        <img className="w-7 h-7" src={fillIn} alt="add Music" />
       </button>
       <ModalForm typeForm={typeForm}/>
     </div>
